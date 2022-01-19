71499828b626715b32c78e77c94ef85881d9b95c
Christian Gonzalez <kgomin@gmail.com>
2022-01-19 18:00:25 +0100

    Lab finished!

--- src/clue.js
+++ src/clue.js
@@ -1,30 +1,229 @@
 // ITERATION 1
 
+const mrGreen = {
+  firstName: 'Jacob',
+  lastName: 'Green',
+  occupation: 'Entrepreneur',
+  age: 45,
+  description: 'He has a lot of connections',
+  image:
+    'https://carboncostume.com/wordpress/wp-content/uploads/2017/10/mrgreen3.jpg',
+  color: 'green '
+};
+
+const drOrchid = {
+  firstName: 'Doctor',
+  lastName: 'Orchid',
+  occupation: 'Scientist',
+  age: 26,
+  description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+  image:
+    'https://images2.minutemediacdn.com/image/upload/c_crop,h_1124,w_2000,x_0,y_50/v1560965041/shape/mentalfloss/586055-hasbro.jpg?itok=x4QQWTxa',
+  color: 'white'
+};
+
+const profPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  occupation: 'Designer',
+  age: 22,
+  description: 'Billionaire video game designer',
+  image:
+    'https://vignette.wikia.nocookie.net/cluedo/images/a/a4/2016C6.jpg/revision/latest?cb=20200927103539',
+  color: 'purple'
+};
+
+const missScarlet = {
+  firstName: 'Kasandra',
+  lastName: 'Scarlet',
+  occupation: 'Actor',
+  age: 31,
+  description: 'She is an A-list movie star with a dark past',
+  image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  color: 'red'
+};
+
+const mrsPeacock = {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  occupation: 'Socialité',
+  age: 36,
+  description:
+    'She is from a wealthy family and uses her status and money to earn popularity',
+  image:
+    'https://vignette.wikia.nocookie.net/cluedo/images/b/b0/2016C5.jpg/revision/latest?cb=20200927103538',
+  color: 'blue'
+};
+
+const mrMustard = {
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  occupation: 'Retired Football player',
+  age: 62,
+  description:
+    'He is a former football player who tries to get by on his former glory',
+  image:
+    'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+  color: 'yellow'
+};
+
 // Suspects Array
+const suspectsArray = [
+  mrGreen,
+  drOrchid,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  mrMustard
+];
 
-const suspectsArray = [];
+// ### Rooms
 
-// Rooms Array
+const diningRoom = {
+  name: 'Dining Room'
+};
+const conservatory = {
+  name: 'Conservatory'
+};
+const kitchen = {
+  name: 'Kitchen'
+};
+const study = {
+  name: 'Study'
+};
+const library = {
+  name: 'Library'
+};
+const billiardRoom = {
+  name: 'Billiard Room'
+};
+const lounge = {
+  name: 'Lounge'
+};
+const ballroom = {
+  name: 'Ballroom'
+};
+const hall = {
+  name: 'Hall'
+};
+const spa = {
+  name: 'Spa'
+};
+const livingRoom = {
+  name: 'Living Room'
+};
+const observatory = {
+  name: 'Observatory'
+};
+const theater = {
+  name: 'Theater'
+};
+const guestHouse = {
+  name: 'Guest House'
+};
+const patio = {
+  name: 'Patio'
+};
 
-const roomsArray = [];
+const roomsArray = [
+  diningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballroom,
+  hall,
+  spa,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+const rope = {
+  name: 'Rope',
+  weight: 10
+};
 
+const knife = {
+  name: 'Knife',
+  weight: 8
+};
+
+const candlestick = {
+  name: 'Candlestick',
+  weight: 2
+};
+
+const dumbbell = {
+  name: 'Dumbbell',
+  weight: 30
+};
+
+const poison = {
+  name: 'Poison',
+  weight: 2
+};
+
+const axe = {
+  name: 'Axe',
+  weight: 15
+};
+
+const bat = {
+  name: 'Bat',
+  weight: 13
+};
+
+const trophy = {
+  name: 'Trophy',
+  weight: 25
+};
+
+const pistol = {
+  name: 'Pistol',
+  weight: 20
+};
+
+const weaponsArray = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+];
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+//console.log(selectRandom(weaponsArray));
 
-function pickMystery() {}
-
+function pickMystery() {
+  const mystery = {
+    suspect: selectRandom(suspectsArray),
+    room: selectRandom(roomsArray),
+    weapon: selectRandom(weaponsArray)
+  };
+  return mystery;
+}
+console.log(pickMystery());
 
 // ITERATION 3
 
-function revealMystery() {}
-
-
+function revealMystery(envelope) {
+  return `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
